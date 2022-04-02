d320132079ba8c6845cd32103e057dc500329962
mandrakodi <81570116+mandrakodi@users.noreply.github.com>
2022-04-02 21:25:21 +0200

    Create kod.py

---
+++ kod.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# XBMC entry point
+# ------------------------------------------------------------
+
+import os
+import sys
+import xbmc
+
+# functions that on kodi 19 moved to xbmcvfs
+try:
+    import xbmcvfs
+    xbmc.translatePath = xbmcvfs.translatePath
+    xbmc.validatePath = xbmcvfs.validatePath
+    xbmc.makeLegalFilename = xbmcvfs.makeLegalFilename
+except:
+    pass
+from platformcode import config, logger
+
+logger.info("init...")
+
+
+librerias = xbmc.translatePath(os.path.join(config.get_runtime_path(), 'lib'))
+sys.path.insert(0, librerias)
+
+if 'elsupremo' in xbmc.getInfoLabel('Container.FolderPath'):
+    from platformcode.platformtools import dialog_ok
+    dialog_ok('Kodi on Demand', 'Non è consentito sfruttare KoD da add-on esterni')
+    exit()
+
+from platformcode import launcher
+
+if sys.argv[2] == "":
+    launcher.start()
+
+launcher.run()
