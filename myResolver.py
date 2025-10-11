268f7e554f2e386b79ad6ca8636aa63675ee84c5
mandrakodi <81570116+mandrakodi@users.noreply.github.com>
2025-10-11 14:45:48 +0200

    Update myResolver.py

--- myResolver.py
+++ myResolver.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals # turns everything to unicode
-versione='1.2.161'
+versione='1.2.162'
 # Module: myResolve
 # Author: ElSupremo
 # Created on: 10.04.2021
-# Last update: 07.10.2025
+# Last update: 11.10.2025
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import re, requests, sys, logging, uuid
@@ -974,7 +974,7 @@ def daddyPremium(codeIn=None):
         'user-agent': randomUa
     }
     s = requests.Session()
-    urlSrv="https://jxoplay.xyz/server_lookup.php?channel_id=premium"+codeIn
+    urlSrv="https://fnjplay.xyz/server_lookup.php?channel_id=premium"+codeIn
     dataJson = s.get(urlSrv, headers=headers)
     arrJ = json.loads(dataJson.text)
     server=arrJ["server_key"]
@@ -987,7 +987,7 @@ def daddyPremium(codeIn=None):
     liz.setMimeType("application/x-mpegURL")
     liz.setProperty('inputstream.adaptive.file_type', 'hls')
     ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0"
-    host="https://jxoplay.xyz"
+    host="https://fnjplay.xyz"
     liz.setProperty('inputstream.adaptive.stream_headers', 'User-Agent='+ua+'&Referer='+host+'/&Origin='+host)
     liz.setProperty('inputstream.adaptive.manifest_headers', 'User-Agent='+ua+'&Referer='+host+'/&Origin='+host)
     return liz
@@ -1117,7 +1117,7 @@ def daddyCode(codeIn=None):
     }
     s = requests.Session()
     
-    urlAuth="https://jxoplay.xyz/premiumtv/daddylive.php?id="+codeIn
+    urlAuth="https://fnjplay.xyz/premiumtv/daddylive.php?id="+codeIn
     
     fu = s.get(urlAuth, headers=headers)
     logga ("AUTH_PAGE: "+fu.text)
@@ -1138,8 +1138,8 @@ def daddyCode(codeIn=None):
 
     headers = {
         'user-agent': randomUa,
-        'referer': "https://jxoplay.xyz/",
-        'origin': "https://jxoplay.xyz"
+        'referer': "https://fnjplay.xyz/",
+        'origin': "https://fnjplay.xyz"
     }
     urlAuth="https://top2new.newkso.ru/auth.php?channel_id=premium"+codeIn+"&ts="+authTs+"&rnd="+authRnd+"&sig="+authSig
     
@@ -1147,7 +1147,7 @@ def daddyCode(codeIn=None):
     dataJ2 = s.get(urlAuth, headers=headers)
     logga("DADDY AUTH "+urlAuth+"\n"+dataJ2.text)
     
-    urlSrv="https://jxoplay.xyz/server_lookup.php?channel_id=premium"+codeIn
+    urlSrv="https://fnjplay.xyz/server_lookup.php?channel_id=premium"+codeIn
 
     dataJson = s.get(urlSrv, headers=headers)
     logga("DADDY JSON "+dataJson.text)
@@ -1156,8 +1156,8 @@ def daddyCode(codeIn=None):
     logga("DADDY_CODE SERVER "+server)
     link="https://"+server+"new.newkso.ru/"+server+"/premium"+codeIn+"/mono.m3u8"
     
-    refe="https://jxoplay.xyz/"
-    origin="https://jxoplay.xyz"
+    refe="https://fnjplay.xyz/"
+    origin="https://fnjplay.xyz"
     
     
     final_url=link+"|Referer="+refe+"&Origin="+origin+"&Connection=Keep-Alive&User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0"
@@ -1550,7 +1550,7 @@ def PlayStream(link):
     }
     s = requests.Session()
     
-    urlAuth="https://jxoplay.xyz/premiumtv/daddylive.php?id="+link
+    urlAuth="https://fnjplay.xyz/premiumtv/daddylive.php?id="+link
     fu = s.get(urlAuth, headers=headers)
     logga ("AUTH_PAGE: "+fu.text)
 
@@ -1570,8 +1570,8 @@ def PlayStream(link):
 
     headers = {
         'user-agent': randomUa,
-        'referer': "https://jxoplay.xyz/",
-        'origin': "https://jxoplay.xyz"
+        'referer': "https://fnjplay.xyz/",
+        'origin': "https://fnjplay.xyz"
     }
     urlAuth="https://top2new.newkso.ru/auth.php?channel_id=premium"+link+"&ts="+authTs+"&rnd="+authRnd+"&sig="+authSig
     dataJ2 = s.get(urlAuth, headers=headers)
@@ -1580,13 +1580,13 @@ def PlayStream(link):
     
     
     
-    urlSrv="https://jxoplay.xyz/server_lookup.php?channel_id=premium"+link
+    urlSrv="https://fnjplay.xyz/server_lookup.php?channel_id=premium"+link
     dataJson = s.get(urlSrv, headers=headers)
     arrJ = json.loads(dataJson.text)
     server=arrJ["server_key"]
     link="https://"+server+"new.newkso.ru/"+server+"/premium"+link+"/mono.m3u8"
-    refe="https://jxoplay.xyz/"
-    origin="https://jxoplay.xyz"
+    refe="https://fnjplay.xyz/"
+    origin="https://fnjplay.xyz"
     
     
     final_url=link+"|Referer="+refe+"&Origin="+origin+"&User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0"
